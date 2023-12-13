4141d93193d8075e5805efb716609c3c1692c8d5
saurabhpathak <saurabhpathak072@gmail.com>
2023-12-13 13:44:23 +0530

    Auth URL

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+const {token} = useContext(AuthContext);
+  useEffect(() => {
+    if(token){
+
+      axios.get('https://react-nativa-app-expense-trac-default-rtdb.firebaseio.com/message.json?auth='+token).then(response=>{
+       setWelcomeMessage(response.data)
+      }).catch(error=>{
+        console.log(error);
+        Alert.alert("Failed to fetch","Failed on welcome screen")
+      })
+    }
+  
+    return () => {
+      setWelcomeMessage("")
+    }
+  }, [])
+  
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{welcomeMessage}</Text>
     </View>
   );
 }
